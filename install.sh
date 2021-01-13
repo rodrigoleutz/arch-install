996ec77b70c7e1e773a3396f43bdc625aefe3a82
Rodrigo Leutz <rodrigo@uware.com.br>
2021-01-13 15:54:02 -0300

    known_hosts

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-mv ~/.ssh/known_hosts ~/.ssh/known_hosts-old
+FILE=~/.ssh/known_hosts
+if [ -f "$FILE" ]; then
+  mv ~/.ssh/known_hosts ~/.ssh/known_hosts-old
+  BOL=1
+fi
 ansible-playbook -i hosts arch-install.yml
+if [ "$BOL" == 1 ]; then
+  rm ~/.ssh/known_hosts
+  mv ~/.ssh/known_hosts-old ~/.ssh/known_hosts
+fi
